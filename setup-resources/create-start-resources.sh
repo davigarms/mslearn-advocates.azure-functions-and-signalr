d4a01ebdbbeee9c9ca0b174dc38bec18bda45cad
Davi Garms <davigarms@gmail.com>
2024-06-28 17:52:24 +0100

    Add SignalR functions

--- setup-resources/create-start-resources.sh
+++ setup-resources/create-start-resources.sh
@@ -33,13 +33,13 @@ printf "Using subscription: ""$SUBSCRIPTION_NAME""\n"
 # Set the resource group name if not provided as a parameter
 RANDOM_STRING=$(openssl rand -hex 5)
 #printf "Using random string: $RANDOM_STRING\n"
-RESOURCE_GROUP_NAME="$USER_NAME-signalr-$RANDOM_STRING"
+RESOURCE_GROUP_NAME="sandbox-dgarms-stock-rg"
 
 # Create a resource group
 az group create \
   --subscription "$SUBSCRIPTION_NAME" \
   --name "$RESOURCE_GROUP_NAME" \
-  --location eastus
+  --location westeurope
 
 # Set default resource group
 az configure --defaults group="$RESOURCE_GROUP_NAME"
---
+++ setup-resources/test.sh
@@ -0,0 +1,2 @@
+example=/var/data
+export example
